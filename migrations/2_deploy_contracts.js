f23fbc945573b97447835dfd469c3375baea80f3
Cruz Molina <cruz.adam.molina@gmail.com>
2020-08-03 10:59:33 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,8 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const MetaCoin = artifacts.require("MetaCoin");
 
-module.exports = async function(deployer, accounts) {
-  await deployer.deploy(ConvertLib);
-  await deployer.link(ConvertLib, MetaCoin);
-  await deployer.deploy(MetaCoin);
-  
-  await metaCoinInstance = await MetaCoin.deployed();
-  // test
-  await metaCoinInstance.sendCoin(accounts[0], 100);
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
